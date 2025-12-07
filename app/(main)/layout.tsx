9e79441d79e41c75db60a548b7551f41b174b3a2
Gopal chandrashekhar Gawas <gopalgawas2004@gmail.com>
2025-12-08 02:26:09 +0530

    latest update

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Analytics } from "@vercel/analytics/next";
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen bg-gray-50 text-white">
@@ -7,6 +7,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
       <main className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         {children}
       </main>
+      <Analytics />
     </div>
   );
 };
